379bad1c6798a5e7b7e3f08d6d9d5ff43cbf4843
viziolileonardo <107259145+viziolileonardo@users.noreply.github.com>
2023-07-21 13:49:56 +0100

    Update rush.js

--- rush.js
+++ rush.js
@@ -2,22 +2,22 @@
 
 // Function to switch between Leonardo and Vincent CVs
 function switchCV() {
-    const leonardoCV = document.getElementById('leonardo-cv');
-    const vincentCV = document.getElementById('vincent-cv');
-    const switchBtn = document.getElementById('switchBtn');
-  
-    // Toggle display of CVs based on their current visibility
-    if (leonardoCV.style.display === 'none') {
-      leonardoCV.style.display = 'block';
-      vincentCV.style.display = 'none';
-      switchBtn.innerText = 'Switch CVs';
-    } else {
-      leonardoCV.style.display = 'none';
-      vincentCV.style.display = 'block';
-      switchBtn.innerText = 'Switch CVs';
-    }
+  const leonardoCV = document.getElementById('leonardo-cv');
+  const vincentCV = document.getElementById('vincent-cv');
+  const switchBtn = document.getElementById('switchBtn');
+
+  // Toggle display of CVs based on their current visibility
+  if (leonardoCV.style.display === 'none') {
+    leonardoCV.style.display = 'block';
+    vincentCV.style.display = 'none';
+    switchBtn.innerText = 'Switch CVs';
+  } else {
+    leonardoCV.style.display = 'none';
+    vincentCV.style.display = 'block';
+    switchBtn.innerText = 'Switch CVs';
   }
-  
-  // Event listener for the "Switch CVs" button
-  document.getElementById('switchBtn').addEventListener('click', switchCV);
+}
+
+// Event listener for the "Switch CVs" button
+document.getElementById('switchBtn').addEventListener('click', switchCV);
   
